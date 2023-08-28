54940ccd9ec74c0644de4db4c86160fd5a241443
bussfromspace <buse.atli_tekgul@nokia-bell-labs.com>
2023-08-29 02:35:13 +0300

    Update fingerprint.py

--- src/fingerprint.py
+++ src/fingerprint.py
@@ -1,5 +1,3 @@
-# Authors: Buse G. A. Tekgul
-# Copyright 2020 Secure Systems Group, Aalto University, https://ssg.aalto.fi
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
